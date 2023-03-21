9f8f2b976faedbe2250e9bdd774c623c6b8a4422
Milosz <miloszloniewski02@gmail.com>
2023-03-21 11:45:16 +0100

    js file added (for coontact sub-page), form modified and headers changed on offer subpage

---
+++ src/js/contactForm.js
@@ -0,0 +1,7 @@
+const firstName = document.querySelector('#name');
+const lastName = document.querySelector('#last-name');
+const email = document.querySelector('#email');
+const sendBtn = document.querySelector('.contact__form-btn');
+const phoneNumber = document.querySelector('#phone-number');
+const textareaMsg = document.querySelector('#msg');
+const allInputs = document.querySelectorAll('input');
